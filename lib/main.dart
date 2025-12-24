194d5461fa4a841eccd9e56beb441efbcdb8586c
corazon4815 <mango4815@gmail.com>
2025-12-24 16:05:19 +0900

    feat : ios, 안드로이드 스플래시 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:catdog/firebase_options.dart';
 import 'package:catdog/ui/pages/login/root_page.dart';
+import 'package:catdog/ui/pages/splash/splash_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -30,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const RootPage(),
+      home: const SplashPage(),
     );
   }
 }
