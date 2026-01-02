c08c338b6e76a67429213e53ad39db03633c2a3e
cwd0217 <cwd0217@naver.com>
2026-01-02 15:58:31 +0900

    feat: 피드뷰 생성 위젯그림 완료

--- lib/ui/pages/feed/view/feed_view.dart
+++ lib/ui/pages/feed/view/feed_view.dart
@@ -6,49 +6,96 @@ class FeedView extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    
     return Scaffold(
+      backgroundColor: const Color(0xFFF5F5F5),
       // appBar: AppBar(),
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              // 제일큰 컨테이너 이미지
-              Row(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 335,
-                    height: 485,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(16),
-                      color: Color(0xffffffff),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(20),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [
-                          Container(
-                            width: 36,
-                            height: 36,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(18),
-                              color: Colors.grey,
-                            ),
-                          ),
-                          Text("닉네임"),
-                        ],
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ],
-          ),
+        child:ListView.builder(
+             itemCount: 10,
+             itemBuilder: (context, index) {
+               return MyFeedCard();
+             },
         ),
       ),
     );
   }
 }
+
+ Widget MyFeedCard(){
+  return Container(
+    // padding: EdgeInsets.symmetric(horizontal: 20),
+    width: double.infinity,
+    margin: const EdgeInsets.only(bottom: 20, left: 20, right: 20),
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(16),
+      color: Colors.white
+    ),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Row(
+            children: [
+              //프로필 이미지 
+              CircleAvatar(radius: 18,backgroundColor: Color(0xffD9D9D9)),
+              SizedBox(width: 10),
+              Text("닉네임",style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              Spacer(),
+              GestureDetector(
+               onTap: () {
+                print("수정 삭제 메뉴 열기");
+               },
+                 child: Icon(Icons.more_vert),
+              )
+              
+            ],
+          ),
+        ),
+        Container(
+          width: double.infinity,
+          height: 300,
+          color: Color(0xffD9D9D9),
+          child: Icon(Icons.image,color: Colors.grey),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                   Icon(Icons.favorite_border, size: 24),
+                   SizedBox(width: 5),
+                   Text("45"),
+                   SizedBox(width: 15),
+                   Icon(Icons.chat_bubble_outline, size: 24),
+                   SizedBox(width: 5),
+                   Text("2"),
+                ],
+              ),
+              SizedBox(height: 10),
+              Text(
+                "고양이가 어떻게 세모네모",
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.w500),
+                maxLines: 2,
+                overflow: TextOverflow.ellipsis,// 글이 넘치면 처리해주는 코드
+              ),
+              const SizedBox(height: 4),
+              Text(
+                "2025년 12월 5일",
+                style: TextStyle(fontSize: 12, color: Colors.grey),
+              ),
+            ],
+          ),
+        )
+      ],
+    ),
+  );
+ }
+ 
